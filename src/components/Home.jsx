b30170acc42a48c7a7a6fc4e9829b5590521cc37
KUMAR <kumar@KUMARs-MacBook-Pro.local>
2023-05-19 11:37:28 +0530

    I made changes in my H1 and the title tag

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row ">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold text-white ">
-            Namaste <br /> I'm a Entrepreneur
+            I,m <br/> a Passionate Developer
           </h2>
           <p className="text-white py-4 max-w-md">
             I'm currently pursuing a Master's degree in Computer Applications at
